9bc9752fd2efab7389b5cbc3f1dad52c5c379f2d
lenovo <lenovo@DESKTOP-F6J9ALJ>
2024-02-27 20:43:49 +0530

    pom surefire updaed

--- demo.automationtesting.in Using Pom/src/main/java/demo/automationtesting/in/pages/RegisterPageUsingPageFactory.java
+++ demo.automationtesting.in Using Pom/src/main/java/demo/automationtesting/in/pages/RegisterPageUsingPageFactory.java
@@ -7,10 +7,10 @@ import org.openqa.selenium.support.FindBy;
 import demo.automationtesting.in.base.BaseClass;
 
 public class RegisterPageUsingPageFactory extends BaseClass {
-	By firstName = By.xpath("//*[@id='basicBootstrapForm']//input");
-	By lastName = By.xpath("//input[@placeholder='Last Name']");
-	By address = By.xpath("//*[@id='basicBootstrapForm']//textarea");
-	By emailid = By.xpath("//*[@id='eid']//input");
-	By phone = By.xpath("//*[@id='basicBootstrapForm']/div[4]//input");
+//	By firstName = By.xpath("//*[@id='basicBootstrapForm']//input");
+//	By lastName = By.xpath("//input[@placeholder='Last Name']");
+//	By address = By.xpath("//*[@id='basicBootstrapForm']//textarea");
+//	By emailid = By.xpath("//*[@id='eid']//input");
+//	By phone = By.xpath("//*[@id='basicBootstrapForm']/div[4]//input");
 
 }
